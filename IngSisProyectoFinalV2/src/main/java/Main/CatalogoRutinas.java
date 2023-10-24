71b9deabc22cf4dce1def4dd2561beff5f84e2bc
Brayton Maccoy <braymaccoy@hotmail.com>
2023-10-24 15:10:26 -0600

    Clases agregadas, adelanto clase rutinas

---
+++ IngSisProyectoFinalV2/src/main/java/Main/CatalogoRutinas.java
@@ -0,0 +1,120 @@
+
+package Main;
+import javax.swing.JOptionPane;
+public class CatalogoRutinas {
+    private int Deporte;
+    private String NombreUsuario;
+    private String contraseña;
+    private String Rutina;
+    private String Rutina2;
+    private String Rutina3;
+
+    public CatalogoRutinas() {
+        this.Deporte = 0;
+        this.NombreUsuario = "";
+        this.contraseña ="" ;
+        this.Rutina = "";
+        this.Rutina2 = "";
+        this.Rutina3 = "";
+        
+        
+    }
+
+    public int getDeporte() {
+        return Deporte;
+    }
+
+    public void setDeporte(int Deporte) {
+        this.Deporte = Deporte;
+    }
+
+    
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRutina() {
+        String rutinaFutbol = "Rutina de entrenamiento para fútbol:\n\n" +
+                "Entrenamiento de fuerza y resistencia:\n" +
+                "1. Sentadillas: 3 series de 10-12 repeticiones.\n" +
+                "2. Flexiones (push-ups): 3 series de 10-12 repeticiones.\n" +
+                "3. Zancadas (lunges): 3 series de 10-12 repeticiones por pierna.\n" +
+                "4. Plancha abdominal: 3 series de 30-60 segundos.\n\n" +
+                "Entrenamiento de agilidad y velocidad:\n" +
+                "1. Carrera en zigzag.\n" +
+                "2. Entrenamiento de sprint: 6-8 sprints de 30-40 metros.\n\n" +
+                "Entrenamiento de resistencia cardiovascular:\n" +
+                "1. Carrera continua: Corre durante 20-30 minutos a un ritmo constante.\n" +
+                "2. Entrenamiento en intervalos: Corre a máxima velocidad durante 30 segundos, seguido de 1-2 minutos de recuperación, repite 5-8 veces.";
+
+        JOptionPane.showMessageDialog(null, rutinaFutbol, "Rutina de entrenamiento para fútbol", JOptionPane.INFORMATION_MESSAGE);
+        return Rutina;
+    }
+
+    public void setRutina(String Rutina) {
+        this.Rutina = Rutina;
+    }
+
+    public String getRutina2() {
+        String rutinaBaloncesto = "Rutina de entrenamiento para baloncesto:\n\n" +
+                "Entrenamiento de fuerza y resistencia:\n" +
+                "1. Sentadillas: 3 series de 10-12 repeticiones.\n" +
+                "2. Flexiones (push-ups): 3 series de 10-12 repeticiones.\n" +
+                "3. Levantamiento de pesas rusas o mancuernas: 3 series de 10-12 repeticiones.\n" +
+                "4. Plancha abdominal: 3 series de 30-60 segundos.\n\n" +
+                "Entrenamiento de agilidad y velocidad:\n" +
+                "1. Ejercicios de dribbling y cambios de dirección.\n" +
+                "2. Saltos verticales: 3 series de 10 repeticiones.\n" +
+                "3. Carrera de intervalos: 8-10 sprints de 30 metros.\n\n" +
+                "Entrenamiento de resistencia cardiovascular:\n" +
+                "1. Carrera continua: Corre durante 20-30 minutos a un ritmo constante.\n" +
+                "2. Entrenamiento en intervalos: Corre a máxima velocidad durante 30 segundos, seguido de 1-2 minutos de recuperación, repite 5-8 veces.";
+
+        JOptionPane.showMessageDialog(null, rutinaBaloncesto, "Rutina de entrenamiento para baloncesto", JOptionPane.INFORMATION_MESSAGE);
+        return Rutina2;
+    }
+
+    public void setRutina2(String Rutina2) {
+        this.Rutina2 = Rutina2;
+    }
+
+    public String getRutina3() {
+         String rutinaVoleibol = "Rutina de entrenamiento para voleibol:\n\n" +
+                "Entrenamiento de fuerza y resistencia:\n" +
+                "1. Sentadillas: 3 series de 10-12 repeticiones.\n" +
+                "2. Flexiones (push-ups): 3 series de 10-12 repeticiones.\n" +
+                "3. Zancadas (lunges): 3 series de 10-12 repeticiones por pierna.\n" +
+                "4. Plancha abdominal: 3 series de 30-60 segundos.\n\n" +
+                "Entrenamiento de agilidad y velocidad:\n" +
+                "1. Ejercicios de salto vertical.\n" +
+                "2. Carrera de agilidad: 8-10 carreras en zigzag.\n" +
+                "3. Saltos laterales: 3 series de 10 repeticiones.\n\n" +
+                "Entrenamiento de resistencia cardiovascular:\n" +
+                "1. Carrera continua: Corre durante 20-30 minutos a un ritmo constante.\n" +
+                "2. Entrenamiento en intervalos: Corre a máxima velocidad durante 30 segundos, seguido de 1-2 minutos de recuperación, repite 5-8 veces.";
+        return Rutina3;
+        
+    }
+
+    public void setRutina3(String Rutina3) {
+        this.Rutina3 = Rutina3;
+    }
+    
+    
+          
+    
+     
+}
--- IngSisProyectoFinalV2/src/main/java/Main/IngSisProyectoFinalV2.java
+++ IngSisProyectoFinalV2/src/main/java/Main/IngSisProyectoFinalV2.java
@@ -1,9 +1,98 @@
+/*Proyecto Introducción a la Programación
+III Cuatrimestre
+Estudiantes:
+            Brayton Maccoy Artola
+            
+            
 
+Profesor: Esteban Marín Chinchilla
+
+ * 
+ */
 package Main;
+import javax.swing.JOptionPane;
 
 public class IngSisProyectoFinalV2 {
 
     public static void main(String[] args) {
-      //Vamos a empezar ;
+      boolean continuar = true;
+      
+      
+      while(continuar){
+            
+            int seleccion = (Integer.parseInt(JOptionPane.showInputDialog("Bienvenido al Menú principal\n"
+                  +"Escoga la opcion que desea ingresar \n1- Agregar Usuario\n2- Catalogo Padres\n3- Catalogo Deportistas\n4- Catalogo Deportes\n5- Catalogo Rutinas\n6- Salir"  )));
+            
+            switch (seleccion) {
+                case 0:
+                    continuar = false;
+                    break;
+
+                case 1:
+                    continuar = false;
+                    break;
+
+                case 2:
+                   
+                    continuar = false;
+                    break;
+                case 3:
+                    
+                    continuar = false;
+                    break;
+                    
+                case 4:
+                    
+                    continuar = false;
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Bienvenido al Catálogo de Rutinasl\n");
+                    CatalogoRutinas cl1 = new CatalogoRutinas();
+                    cl1.setNombreUsuario(JOptionPane.showInputDialog("Ingrese su nombre de usuario: "));
+                    cl1.setContraseña(JOptionPane.showInputDialog("Ingrese su contaseña: "));
+                    cl1.setDeporte(Integer.parseInt(JOptionPane.showInputDialog("Seleccione el deporte al que va ingresar \n1- Futball\n2- Basketball \n3- Volleyball")));
+                    int deporte = cl1.getDeporte();
+                    switch (deporte) {
+                    
+                        
+                    case 0:
+                    
+                    break;   
+                        
+                    case 1:
+                    JOptionPane.showMessageDialog(null, "Bienvenido a la Rutina de Futball\n");
+                    cl1.getRutina();
+                    break;
+
+                    case 2:
+                    JOptionPane.showMessageDialog(null, "Bienvenido a la rutina de Basketball\n");
+                    cl1.getRutina2();
+                    break;
+
+                    case 3:
+                    JOptionPane.showMessageDialog(null, "Bienvenido a la rutina de Volleybal\n");
+                    cl1.getRutina3();
+                    
+                    break;
+                    
+                    default:
+                    // Opción no válida
+                    
+                    JOptionPane.showMessageDialog(null, "Opción no válida");
+                    break;
+                    
+                    }
+                   
+                    
+                    continuar = false;
+                    break;
+
+                default:
+                    // Opción no válida
+                    
+                    JOptionPane.showMessageDialog(null, "Opción no válida");
+                    break;
+            }
+      }
     }
 }
